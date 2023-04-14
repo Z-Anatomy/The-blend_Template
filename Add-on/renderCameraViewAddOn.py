363d66672b5e34bb201ced63be0ff0479a559eda
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-04-14 14:26:37 +0530

    new update

--- Add-on/renderCameraViewAddOn.py
+++ Add-on/renderCameraViewAddOn.py
@@ -5,13 +5,12 @@ bl_info = {
     "name": "Render Image",
     "author": "Pratik Sharma(@biomathcode)",
     "version": (1, 0),
-    "blender": (3, 2, 0),
+    "blender": (3, 0, 0),
     "location": "",
     "description": "Render Image in the ViewPort of the Camera",
     "warning": "Render the View of the Camera Object",
-    "wiki_url": "https://github.com/Z-Anatomy/blend-template",
-    "category": "Add Mesh",
-
+    "wiki_url": "",
+    "category": "Render",
 }
 
 
@@ -21,101 +20,76 @@ class RenderImageOperator(bpy.types.Operator):
     bl_label = "Render Image"
 
     file_format: bpy.props.EnumProperty(
-        items=[("PNG", "PNG", "PNG"), ("JPEG", "JPEG", "JPEG"),
-               ("BMP", "BMP", "BMP")],
         name="File Format",
-        default="PNG",
+        items=[('PNG', 'PNG', 'Portable Network Graphics'),
+               ('JPEG', 'JPEG', 'Joint Photographic Experts Group'),
+               ('BMP', 'BMP', 'Bitmap')],
+        default='PNG'
     )
-    file_path: bpy.props.StringProperty(
-        name="File Path", subtype="FILE_PATH", default="//render.png")
+    camera: bpy.props.StringProperty(
+        name="Camera",
+        default="Camera"
+    )
+
+    filepath: bpy.props.StringProperty(
+        name="File Path",
+        description="Enter the file path where the image should be saved",
+        subtype="FILE_PATH",
+        default="/Users/coolhead/Desktop/render.png",
+    )
 
     def execute(self, context):
-        try:
-            # Use context overrides to get the selected camera object
-            override = context.copy()
-            override["area.type"] = "VIEW_3D"
-            override["region.type"] = "WINDOW"
-            override["space_data.region_3d.view_perspective"] = "CAMERA"
-            camera = context.scene.camera = context.space_data.camera
-            if camera is None:
-                self.report({"ERROR"}, "No camera selected")
-                return {"CANCELLED"}
+        # Set the render engine to use (optional, defaults to "BLENDER_EEVEE")
+        context.scene.render.engine = "BLENDER_EEVEE"
+        context.scene.render.image_settings.file_format = self.file_format
 
-            # Set the render engine to use (optional, defaults to "BLENDER_EEVEE")
-            context.scene.render.engine = "BLENDER_EEVEE"
-            context.scene.render.image_settings.file_format = self.file_format
+        # Set the resolution of the output image (optional, defaults to 1920x1080)
+        context.scene.render.resolution_x = 1920
+        context.scene.render.resolution_y = 1080
 
-            # Set the resolution of the output image (optional, defaults to 1920x1080)
-            context.scene.render.resolution_x = 1920
-            context.scene.render.resolution_y = 1080
+        # Set the camera to use for rendering (optional, defaults to the active camera)
+        camera = context.scene.camera
+        context.scene.render.filepath = self.filepath
 
-            # Set the camera to use for rendering
-            context.scene.camera = camera
+        # Render the current camera view as an image
+        bpy.ops.render.render(write_still=True, use_viewport=True)
 
-            # Set the filepath for the output image
-            context.scene.render.filepath = self.file_path
-
-            # Render the current camera view as an image
-            bpy.ops.render.render(
-                write_still=True, use_viewport=True, override=override)
-
-            self.report({"INFO"}, "Rendering complete")
-            return {"FINISHED"}
-
-        except Exception as e:
-            self.report({"ERROR"}, str(e))
-            return {"CANCELLED"}
+        return {'FINISHED'}
 
 
-# class RenderImageOperator(bpy.types.Operator):
-#     """Render the current camera view as an image"""
-#     bl_idname = "render.render_image"
-#     bl_label = "Render Image"
-
-#     def execute(self, context):
-#         # Set the render engine to use (optional, defaults to "BLENDER_EEVEE")
-#         context.scene.render.engine = "BLENDER_EEVEE"
-#         context.scene.render.image_settings.file_format = "PNG"
-
-#         # Set the resolution of the output image (optional, defaults to 1920x1080)
-#         context.scene.render.resolution_x = 1920
-#         context.scene.render.resolution_y = 1080
-
-#         # Set the camera to use for rendering (optional, defaults to the active camera)
-#         camera = bpy.data.objects["Camera"]
-#         context.scene.camera = camera
-
-#         context.scene.render.filepath = "//data.png"
-
-#         # Render the current camera view as an image
-#         bpy.ops.render.render(write_still=True, use_viewport=True)
-
-#         return {'FINISHED'}
-
-
-class RenderImageView(bpy.types.Panel):
+class RenderImagePanel(bpy.types.Panel):
 
     bl_space_type = 'VIEW_3D'
-    bl_idname = "PT_RenderImage"
     bl_region_type = 'UI'
     bl_label = 'Render Image'
-    bl_context = 'objectmode'
-
-    bl_category = 'Render Image'
+    bl_category = 'Render'
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("render.render_image")
+        row = layout.row()
+        row.prop(context.scene.render, "filepath", text="File Path")
+        row = layout.row()
+        row.prop(context.scene.render,
+                 "image_settings.file_format", text="File Format")
+        row = layout.row()
+        row.operator("render.render_image",
+                     text="Render Image")
+
+
+classes = [
+    RenderImageOperator,
+    RenderImagePanel,
+]
 
 
 def register():
-    bpy.utils.register_class(RenderImageOperator)
-    bpy.utils.register_class(RenderImageView)
+    for cls in classes:
+        bpy.utils.register_class(cls)
 
 
 def unregister():
-    bpy.utils.unregister_class(RenderImageOperator)
-    bpy.utils.register_class(RenderImageView)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
 
 
 if __name__ == "__main__":
--- scripts/renderAddon.py
+++ scripts/renderAddon.py
@@ -3,7 +3,7 @@ import bpy
 
 bl_info = {
     "name": "Render Image",
-    "author": "Your Name Here",
+    "author": "Pratik Sharma(@biomathcode)",
     "version": (1, 0),
     "blender": (3, 0, 0),
     "location": "",
@@ -14,30 +14,43 @@ bl_info = {
 }
 
 
+class SetActiveCameraOperator(bpy.types.Operator):
+    """Set the selected camera as the active camera"""
+    bl_idname = "render.set_active_camera"
+    bl_label = "Set Active Camera"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        active_object = context.view_layer.objects.active
+        if active_object is None or active_object.type != 'CAMERA':
+            self.report({'ERROR'}, "No camera selected")
+            return {'CANCELLED'}
+
+        context.scene.camera = active_object
+        self.report({'INFO'}, "Camera set as active")
+        return {'FINISHED'}
+
+
 class RenderImageOperator(bpy.types.Operator):
     """Render the current camera view as an image"""
     bl_idname = "render.render_image"
     bl_label = "Render Image"
 
-    file_format: bpy.props.EnumProperty(
-        name="File Format",
-        items=[('PNG', 'PNG', 'Portable Network Graphics'),
-               ('JPEG', 'JPEG', 'Joint Photographic Experts Group'),
-               ('BMP', 'BMP', 'Bitmap')],
-        default='PNG'
-    )
+    def filePathUpdate(self, context):
+        context.scene.render.filepath = self.filepath
 
-    file_path: bpy.props.StringProperty(
-        name="File Path",
+    filepath: bpy.props.StringProperty(
+        name="FilePath",
         description="Enter the file path where the image should be saved",
         subtype="FILE_PATH",
         default="/Users/coolhead/Desktop/render.png",
+        update=filePathUpdate
     )
 
     def execute(self, context):
         # Set the render engine to use (optional, defaults to "BLENDER_EEVEE")
         context.scene.render.engine = "BLENDER_EEVEE"
-        context.scene.render.image_settings.file_format = self.file_format
+        context.scene.render.image_settings.file_format = "PNG"
 
         # Set the resolution of the output image (optional, defaults to 1920x1080)
         context.scene.render.resolution_x = 1920
@@ -45,7 +58,7 @@ class RenderImageOperator(bpy.types.Operator):
 
         # Set the camera to use for rendering (optional, defaults to the active camera)
         camera = context.scene.camera
-        context.scene.render.filepath = self.file_path
+        # context.scene.render.filepath = self.filepath
 
         # Render the current camera view as an image
         bpy.ops.render.render(write_still=True, use_viewport=True)
@@ -63,18 +76,19 @@ class RenderImagePanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         row = layout.row()
-        row.prop(context.scene.render, "filepath", text="File Path")
-        row = layout.row()
-        row.prop(context.scene.render,
-                 "image_settings.file_format", text="File Format")
+        row.prop(context.scene.render, "filepath", text="File Format")
+
         row = layout.row()
         row.operator("render.render_image",
                      text="Render Image")
+        row = layout.row()
+        row.operator("render.set_active_camera", text="Set Active Camera")
 
 
 classes = [
     RenderImageOperator,
     RenderImagePanel,
+    SetActiveCameraOperator
 ]
 
 
